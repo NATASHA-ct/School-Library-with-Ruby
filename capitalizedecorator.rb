81d901bd705d3d86f08d492286c7f396c18f0086
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-05 22:00:44 +0300

    Add capitalize checking method to capitalize decorator

--- capitalizedecorator.rb
+++ capitalizedecorator.rb
@@ -1,5 +1,8 @@
 require './base_decorator'
 
 class CapitalizeDecorator < Decorator
-  
+#method to make sure output of nameable is capitalized
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
