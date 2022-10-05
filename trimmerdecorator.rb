b8d2018c1b2ec6835e38390b16960d3982913170
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-05 22:03:51 +0300

    Add trimmer decorator with its method to check for size

---
+++ trimmerdecorator.rb
@@ -0,0 +1,8 @@
+require './base_decorator'
+
+class TrimmerDecorator < Decorator
+#method to make sure output of nameable is capitalized
+  def correct_name
+    @nameable.correct_name[0...10]
+  end
+end
