f194bda446a62f4e7cdb19d09d8a314003b75313
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-06 23:29:08 +0300

    Fix linter error

--- book.rb
+++ book.rb
@@ -1,8 +1,8 @@
 class Book
-  attr_accessor :title :author
+  attr_accessor :title, :author
   attr_reader :rental
 
-  def initialize(title ,author)
+  def initialize(title, author)
     @title = title
     @author = author
     @rental = []
--- rental.rb
+++ rental.rb
@@ -1,15 +1,13 @@
 class Rental
-  attr_accessor :date , :person ,:book 
-  
-  def initialize(date , person , book)
+  attr_accessor :date, :person, :book
+
+  def initialize(date, person, book)
     @date = date
-    #belongs-to side of rental and person
+    # belongs-to side of rental and person
     person.rentals << self
     @person = person
-    #belongs-to side of rental and book
+    # belongs-to side of rental and book
     book.rentals << self
     @book = book
   end
-
-
 end
