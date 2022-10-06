fa1756d9d906d4ca38dd2410617fd29e722a4c2f
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-07 00:54:37 +0300

    Add menu to the main file

---
+++ main.rb
@@ -0,0 +1,21 @@
+d#!/usr/bin/env ruby
+
+require_relative './app'
+@method = App.new
+
+def print_menu
+  print 'Welcome to my Library! You may perform the following tasks:
+  1. List all books.
+  2. List all people
+  3. Create a person
+  4. Create a book.
+  5. Create a rental
+  6. List all rentals for a given person id.
+  7. Exit
+  Choose (1-7): '
+end
+
+def user_choice
+  gets.chomp.to_i
+end
+
